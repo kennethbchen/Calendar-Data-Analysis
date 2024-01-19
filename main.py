222b3742c000cec95c7fe54aa9388b4cc77c2e64
kennethbchen <kennethbchen@gmail.com>
2024-01-19 12:39:14 -0500

    Implemented parsing start, end, timezone, and duration data

--- main.py
+++ main.py
@@ -2,12 +2,17 @@
 import datetime
 import json
 import os.path
+import pandas as pd
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+
+from pyrfc3339 import parse as rfc_parse
+
+pd.options.display.width = 0
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
@@ -69,7 +74,7 @@ def request(creds):
                 calendarId=calendar_id,
                 timeMin=datetime.datetime(2018, 1, 1).isoformat() + "Z",
                 timeMax=datetime.datetime.utcnow().isoformat() + "Z",
-                maxResults=2500,
+                maxResults=10,
                 pageToken=next_page_token,
                 orderBy="startTime",
                 singleEvents=True,
@@ -79,10 +84,12 @@ def request(creds):
         events.extend(events_result.get("items", []))
 
         if "nextPageToken" in events_result:
+            break
             next_page_token = events_result["nextPageToken"]  # Still more events to get, make another request
         else:
             break
 
+    # data = pd.DataFrame(columns=["Name", "Start", "End", "Duration"])
 
     print(events[0])
 
@@ -91,10 +98,29 @@ def request(creds):
     print(events[-1])
 
     print(len(events))
+
+
+    data = pd.DataFrame.from_records(events, columns=["summary", "start", "end"])
+
+    # Unpack start and end dicts into columns
+    # Rename columns, so they are not duplicated
+    # Drop this timeZone value because we will get it later
+    data = pd.concat([data, pd.json_normalize(data["start"]).drop("timeZone", axis=1).rename(columns={"dateTime": "startTime"})], axis=1)
+    data.drop("start", axis=1, inplace=True)
+
+    data = pd.concat([data, pd.json_normalize(data["end"]).rename(columns={"dateTime": "endTime"})], axis=1)
+    data.drop("end", axis=1, inplace=True)
+
+    print(rfc_parse(data.iloc[0]["endTime"]) - rfc_parse(data.iloc[0]["startTime"]))
+
+    data["startTime"] = data["startTime"].apply(rfc_parse)
+    data["endTime"] = data["endTime"].apply(rfc_parse)
+
+    data["delta"] = data["endTime"] - data["startTime"]
+
+    print(data)
+
     return
-    for event in events:
-        start = event["start"].get("dateTime", event["start"].get("date"))
-        print(start, event["summary"])
 
 if __name__ == "__main__":
     main()
